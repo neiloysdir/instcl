2b5dee5193d71d89e81dec2ada45769d03e14682
neiloysdir <neiloyhassan1@gmail.com>
2022-07-02 13:04:45 +0600

    responsive init

---
+++ lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MobileScreenLayout extends StatefulWidget {
+  MobileScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('This is for mobile screen'),
+      ),
+    );
+  }
+}
---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:instcl/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatefulWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  ResponsiveLayout({
+    Key? key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout,
+  }) : super(key: key);
+
+  @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return widget.webScreenLayout;
+        } else {
+          return widget.mobileScreenLayout;
+        }
+      },
+    );
+  }
+}
